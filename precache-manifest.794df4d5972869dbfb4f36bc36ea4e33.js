8766a7f371931757fda0f785023d6bb5c91c346b
muhsin7majeed <muhsin7ak@gmail.com>
2019-09-22 22:29:33 +0530

    Updates

---
+++ precache-manifest.794df4d5972869dbfb4f36bc36ea4e33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc771a60999a80d29f9e303b0c6227cb",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "558afdf98a4160b1623e",
+    "url": "/react-todo/static/css/main.2078b410.chunk.css"
+  },
+  {
+    "revision": "a7e384f473fd7f4f28de",
+    "url": "/react-todo/static/js/2.3c834239.chunk.js"
+  },
+  {
+    "revision": "558afdf98a4160b1623e",
+    "url": "/react-todo/static/js/main.c5d348d8.chunk.js"
+  },
+  {
+    "revision": "45f774123badc23ed67a",
+    "url": "/react-todo/static/js/runtime~main.e839b18e.js"
+  }
+]);
